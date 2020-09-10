7db027d7aa4a4d330be7553c405306b532d38800
KESHIA VEREDA <vereda761@gmail.com>
2020-09-09 21:03:27 -0700

    object entries

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+console.log(data);
+
+//loop
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+});
+
+//append
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+
+//entries
+Object.entries(ufoReport).forEach(function([key,value]) {
+    console.log(key, value);
+});
+});
